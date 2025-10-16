dcc6318701d5cd0376a776b877176602c4dfccb9
ngoctienz <tien2k6v1@gmail.com>
2025-10-16 21:26:37 +0700

    PUSH

---
+++ 6551071079_JAVASCRIPT_Câu 1/Scripts/Scripts.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $("#jsstyle").click(function(){
+//         $("#text").css({
+//             "font-size": "20px",
+//             "font-family": "Arial",
+//             "color": "red"
+//         })
+//     })
+// })
